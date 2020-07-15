6ff9068b2c35345d4a4740b196381b733e01aae5
toofaniCoder <smartcoder500@gmail.com>
2020-07-15 13:51:51 +0530

    intial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="has-background-primary">
+    <section>
+      <div class="container py-6 px-6">
+        <div class="columns">
+          <div class="column is-4" v-for="pricing in pricings" v-bind:key="pricing.id">
+            <PricingTable>
+              <template slot="header">
+                <h1 class="title is-1">{{pricing.plan}}</h1>
+                <h2 class="subtitle is-5">{{pricing.price}}</h2>
+              </template>
+              <template slot="body">
+                <ul>
+                  <li v-for="(feature, index) in pricing.features" v-bind:key="index">{{feature}}</li>
+                </ul>
+              </template>
+              <template slot="footer">
+                <button class="button is-primary mt-5">Buy Now</button>
+              </template>
+            </PricingTable>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PricingTable from "./components/PricingTable";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: { PricingTable },
+  data() {
+    return {
+      pricings: [
+        {
+          id: 1,
+          plan: "FREE",
+          price: "$0/month",
+          features: [
+            "Single User",
+            "5GB Storage",
+            "Unlimited Public Projects",
+            "Community Access",
+            "Unlimited Private Projects",
+            "Dedicated Phone Support",
+            "Free 1 Subdomain",
+            "Weekly Status Reports"
+          ]
+        },
+        {
+          id: 2,
+          plan: "PLUS",
+          price: "$9/month",
+          features: [
+            "5 Users",
+            "50GB Storage",
+            "Unlimited Public Projects",
+            "Community Access",
+            "Unlimited Private Projects",
+            "Dedicated Phone Support",
+            "Free 2 Subdomain",
+            "Monthly Status Reports"
+          ]
+        },
+        {
+          id: 3,
+          plan: "PRO",
+          price: "$49/month",
+          features: [
+            "Unlimited Users",
+            "150GB Storage",
+            "Unlimited Public Projects",
+            "Community Access",
+            "Unlimited Private Projects",
+            "Dedicated Phone Support",
+            "Unlimited Free Subdomain",
+            "Monthly Status Reports"
+          ]
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+}
+body {
+  font-family: "Roboto";
+}
+ul {
+  line-height: 200%;
 }
 </style>
---
+++ src/components/PricingTable.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="box has-text-centered">
+    <slot name="header" />
+    <slot name="body" />
+    <slot name="footer" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PricingTable"
+};
+</script>
